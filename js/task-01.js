06e999477322a79421f5b454fb36dab8e44d4c38
apazient <nataliia.amanzhayeva@web.de>
2023-01-22 13:43:19 +0100

    create respository

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const categories = document.querySelectorAll(".item").length;
+const catName= document.getElementsByClassName("")
+
+
+console.log(`Number of categories: ${categories}`)
+console.log(document.querySelector(".item"))
